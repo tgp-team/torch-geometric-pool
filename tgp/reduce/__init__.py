d3ce070d6c8512a8afe9e3198e6f621f67dd1d09
Carlo <carlo.bta@gmail.com>
2025-09-30 17:26:26 +0200

    red docs consistent with sel and con

--- tgp/reduce/__init__.py
+++ tgp/reduce/__init__.py
@@ -1,3 +1,16 @@
 from .base_reduce import BaseReduce, Reduce
 from .global_reduce import dense_global_reduce, global_reduce
 from .identity_reduce import IdentityReduce
+
+reduce_functions = [
+    "global_reduce",
+    "dense_global_reduce",
+]
+
+reduce_classes = [
+    "Reduce",
+    "BaseReduce",
+    "IdentityReduce",
+]
+
+__all__ = reduce_classes + reduce_functions
