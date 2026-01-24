b4d242fbdaf0f097ea5a76e5e90abe4511ab96b1
Filippo Maria Bianchi <filippo.m.bianchi@uit.no>
2026-01-24 21:26:19 +0100

    reorganized test folder organization, factorized some common functions, and started to work on general test restructuring (part 7)

---
+++ tests/data/test_collate.py
@@ -0,0 +1,15 @@
+"""Placeholder test file for collate module.
+
+TODO: Add tests for tgp.data.collate
+"""
+
+import pytest
+
+# TODO: Import and test collate functions
+# from tgp.data.collate import ...
+
+
+@pytest.mark.skip(reason="Tests not yet implemented")
+def test_collate_placeholder():
+    """Placeholder test for collate module."""
+    pass
---
+++ tests/datasets/test_expwl1.py
@@ -0,0 +1,15 @@
+"""Placeholder test file for ExpWL1 dataset.
+
+TODO: Add tests for tgp.datasets.expwl1
+"""
+
+import pytest
+
+# TODO: Import and test ExpWL1 dataset
+# from tgp.datasets.expwl1 import ExpWL1
+
+
+@pytest.mark.skip(reason="Tests not yet implemented")
+def test_expwl1_placeholder():
+    """Placeholder test for ExpWL1 dataset."""
+    pass
